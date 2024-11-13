c754fd1f61516113561b61314ac8c581d614bcff
italo-gus <italogus@gmail.com>
2024-11-13 17:25:15 -0300

    W3 001 HelloWorld

---
+++ w3schools/goHome/helloworld.go
@@ -0,0 +1,8 @@
+package main // Define o pacote
+import (
+	"fmt"
+) // importa pacote / biblioteca
+
+func main() { // Cria função sem parâmetros e sem retorno
+	fmt.Println("Hello World!") // utiliza a função Println (imprimir linha em tela ) da biblioteca/pacote o texto (string) "Hello World!"
+}
