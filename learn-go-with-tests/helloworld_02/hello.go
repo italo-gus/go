ded7069886a790c9dc630d53741ad87bdcdc15fe
italo-gus <italogus@gmail.com>
2024-11-14 18:17:24 -0300

    006 GOTDD helloworld02 func

--- learn-go-with-tests/helloworld_02/hello.go
+++ learn-go-with-tests/helloworld_02/hello.go
@@ -6,8 +6,13 @@ https://pkg.go.dev/fmt@go1.23.3
 
 import "fmt" // Importar Biblioteca / Pacote
 
+// Criação da Função Hello (sem parâmetros ) tipo de retorno string {}
+func Hello() string {
+	return " Hello, world"
+}
+
 // Criação de Função func nome_da_Função (parâmetros nome e tipo ) tipo de retorno {}
 // Criação de Função main (sem parâmetros) sem retorno {}
 func main() {
-	fmt.Println("Hello, world") // imprimir em tela (stdout [saída padrão do terminal]) o texto "Hello, world" // https://pkg.go.dev/fmt#Println
+	fmt.Println(Hello()) // imprimir em tela (stdout [saída padrão do terminal]) o retorno da função Hello
 }
