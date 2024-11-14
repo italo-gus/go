04aa60ad7a68863f3e4188ba24fbab3ab59803a7
italo-gus <italogus@gmail.com>
2024-11-14 12:44:07 -0300

    004 W3 Declaração de variáveis

--- w3schools/goHome/helloworld.go
+++ w3schools/goHome/helloworld.go
@@ -6,3 +6,11 @@ import (
 func main() { // Cria função sem parâmetros e sem retorno
 	fmt.Println("Hello World!") // utiliza a função Println (imprimir linha em tela ) da biblioteca/pacote o texto (string) "Hello World!"
 }
+
+// sintexe go código mais compacto (isso não é recomendado porque torna o código mais difícil de ler)
+// package main; import ("fmt"); func main() { fmt.Println("Hello World!");}
+
+// comntário de uma linha
+/* Início
+Comentário de mais de uma linha
+Fim */
