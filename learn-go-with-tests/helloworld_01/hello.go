2d057dc93b7a66507d914f95d438c9163ed31c12
italo-gus <italogus@gmail.com>
2024-11-14 17:43:34 -0300

    006 GOTDD Criando Histórico

---
+++ learn-go-with-tests/helloworld_01/hello.go
@@ -0,0 +1,12 @@
+package main // Definição do pacote
+
+/* fmt O pacote fmt implementa E/S formatadas com funções análogas a printf e scanf do C. Os 'verbos' de formato são derivados do C, mas são mais simples.
+https://pkg.go.dev/fmt@go1.23.3
+*/
+
+import "fmt" // Importar Biblioteca / Pacote
+
+// Criação de Função func nome_da_Função () tipo de retorno {} sem parâmetros
+func main() {
+	fmt.Println("Hello, world") // imprimir em tela o texto "Hello, world"
+}
