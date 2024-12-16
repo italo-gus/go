27e0727149a6da9a1e0758b26d970904b1e2c7e6
italo-gus <italogus@gmail.com>
2024-12-16 15:53:28 -0300

    040 GOTDD maps_04 - Refactor - Refatoração

---
+++ learn-go-with-tests/maps_04/dictionary.go
@@ -0,0 +1,43 @@
+package maps_0 // Evitar erro do tipo pânico em tempo de execução devido tipo dados mapa nula (nil)
+
+import "errors"
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+type Dictionary map[string]string
+
+/*
+func (d Dictionary) Search(word string) string {
+	return d[word]
+}
+*/
+
+// função com retorno de tratamento de erro
+/*
+func (d Dictionary) Search(word string) (string, error) {
+	return d[word], nil
+}
+*/
+
+/*
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
+}
+*/
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
